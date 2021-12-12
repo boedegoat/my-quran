654a1e0c81b0f565cf92f48d4f8e72aee3087b2f
boedegoat <bhremada.fka@gmail.com>
2021-12-12 09:21:48 +0700

    Configure absolute path

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
 
 export default function NotFound404Page() {
   return <Layout title='404 Page Not Found' navbarTitle='404'></Layout>
