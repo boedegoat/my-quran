20f37ea6ada87ecea01661e647f5c48d9682c81e
boedegoat <bhremada.fka@gmail.com>
2021-12-15 00:04:25 +0700

    Make BigLink component

---
+++ components/BigLink.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function BigLink({ href, className, emoji, label, description }) {
+  return (
+    <Link href={href}>
+      <a className={`shadow-md p-5 rounded-lg ${className}`}>
+        <h1 className='text-2xl mb-2'>
+          {emoji} <span className='text-slate-900 font-bold'>{label}</span>
+        </h1>
+        <p className='text-slate-500'>{description}</p>
+      </a>
+    </Link>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import BigLink from 'components/BigLink'
 import Layout from 'components/Layout'
 import { useTimeGreeting } from 'hooks'
 
@@ -23,40 +24,27 @@ export default function Home() {
         </h1>
       </section>
 
-      <section className='mt-5 shadow-md p-5 rounded-lg'>
-        <h1 className='text-2xl mb-2'>
-          ▶ <span className='text-slate-900 font-bold'>Continue Read</span>
-        </h1>
-        <p className='text-slate-500'>QS: Al-Baqarah 30</p>
-      </section>
-
-      <div className='grid sm:grid-cols-2 sm:grid-rows-3 gap-5'>
-        <section className='mt-5 shadow-md p-5 rounded-lg row-span-full'>
-          <h1 className='text-2xl mb-2'>
-            📖 <span className='text-slate-900 font-bold'>Read Quran</span>
-          </h1>
-          <p className='text-slate-500'>Explore 30 juz of al quran</p>
-        </section>
-        <section className='mt-5 shadow-md p-5 rounded-lg'>
-          <h1 className='text-2xl mb-2'>
-            ❤ <span className='text-slate-900 font-bold'>Your Favorites</span>
-          </h1>
-          <p className='text-slate-500'>See your favorite surah and ayat</p>
-        </section>
-        <section className='mt-5 shadow-md p-5 rounded-lg'>
-          <h1 className='text-2xl mb-2'>
-            💸 <span className='text-slate-900 font-bold'>Donate</span>
-          </h1>
-          <p className='text-slate-500'>Donate to others</p>
-        </section>
-        <section className='mt-5 shadow-md p-5 rounded-lg'>
-          <h1 className='text-2xl mb-2'>
-            ⚙ <span className='text-slate-900 font-bold'>Settings</span>
-          </h1>
-          <p className='text-slate-500'>Change font size, theme, and more</p>
-        </section>
+      <div className='grid mt-5 grid-cols-2 grid-rows-2 gap-5'>
+        <BigLink
+          className='row-span-full'
+          href='/read-quran'
+          emoji='▶'
+          label='Continue Read'
+          description='QS: Al-Baqarah 30'
+        />
+        <BigLink
+          href='/read-quran'
+          emoji='📖'
+          label='Read Quran'
+          description='Explore 30 juz of al quran'
+        />
+        <BigLink
+          href='/bookmark/favorites'
+          emoji='❤'
+          label='Your Favorites'
+          description='See your favorite surah and ayat'
+        />
       </div>
-      {/* bookmark */}
     </Layout>
   )
 }
