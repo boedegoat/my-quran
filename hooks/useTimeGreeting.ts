c9c7d4ab88d64a1ca8f79652617216d9fe0506b2
boedegoat <bhremada.fka@gmail.com>
2021-12-22 10:32:47 +0700

    Refactor useTimeGreeting

--- hooks/useTimeGreeting.ts
+++ hooks/useTimeGreeting.ts
@@ -3,13 +3,17 @@ import { useEffect, useState } from 'react'
 export function useTimeGreeting() {
   const [greeting, setGreeting] = useState('')
 
-  useEffect(() => {
+  const handleGreeting = () => {
     const today = new Date()
     const hour = today.getHours()
-    if (hour >= 0 && hour <= 6) setGreeting('Good Midnight')
-    else if (hour >= 6 && hour <= 12) setGreeting('Good Morning')
-    else if (hour >= 12 && hour <= 17) setGreeting('Good Afternoon')
+    if (hour >= 0 && hour < 6) setGreeting('Good Midnight')
+    else if (hour >= 6 && hour < 12) setGreeting('Good Morning')
+    else if (hour >= 12 && hour < 17) setGreeting('Good Afternoon')
     else setGreeting('Good Evening')
+  }
+
+  useEffect(() => {
+    handleGreeting()
   }, [])
 
   return greeting
